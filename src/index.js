2a4064fbb6eecfb093d94272022c39fba0d11b6f
Luiz Fernando Cardozo <luizfcardozo@gmail.com>
2019-05-24 17:28:58 -0300

    contador

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React, { Component, Fragment } from 'react';
+import { render } from 'react-dom';
+
+import Button from './Button';
+
+class App extends Component {
+    state = {
+        counter: 0,
+    };
+
+    handleClick = () => {
+        this.setState({ counter: this.state.counter + 1 });
+    };
+
+    render() {
+        return (
+            <Fragment>
+                <h1>Teste</h1>
+                <h2>{this.state.counter}</h2>
+                <Button onClick={this.handleClick}> Somar</Button>
+            </Fragment>
+        );
+    }
+}
+
+render(<App />, document.getElementById('app'));
